73928783e00cf6efd7b3c63d71f747ce55085449
privera <p.riverah@uniandes.edu.co>
2025-10-18 01:25:08 -0500

    Fix para tener ALB

--- terraform/modules/beanstalk/outputs.tf
+++ terraform/modules/beanstalk/outputs.tf
@@ -5,3 +5,7 @@ output "application_name" {
 output "endpoint_url" {
     value = aws_elastic_beanstalk_environment.env.endpoint_url
 }
+
+output "dns_url" {
+    value = aws_elastic_beanstalk_environment.env.cname
+}
--- terraform/stacks/BeanstalkApp/outputs.tf
+++ terraform/stacks/BeanstalkApp/outputs.tf
@@ -5,3 +5,7 @@ output "application_name" {
 output "endpoint_url" {
     value = module.beanstalk.endpoint_url
 }
+
+output "dbs_url" {
+    value = module.beanstalk.dns_url
+}
